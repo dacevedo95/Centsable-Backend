62893088a6aa3c8c4a298b7b9aa5832a42d19322
David Acevedo <dacevedo@prosightspecialty.com>
2020-02-07 12:34:46 -0500

    Added logging to user endpoints and comments

--- app/api/users.py
+++ app/api/users.py
@@ -11,42 +11,72 @@ import json
 @bp.route('/user/exists', methods=['GET'])
 def check_user_exists():
     try:
+        # Gets the parameters for the call and
+        # Checks whether the 'phoneNumber' parameter has been included and is not empty
         params = request.args
-        exist_response = dict()
-
-        if 'phoneNumber' not in params:
+        if 'phoneNumber' not in params or params['phoneNumber'] == "" or params['phoneNumber'] == None:
+            current_app.logger.error('phoneNumber not included in request arguements')
             return error_response(400)
 
-        user_by_phone = User.query.filter_by(phone_number=params['phoneNumber']).first()
-        if user_by_phone != None:
-            exist_response['exists'] = True
-            exist_response['phoneExists'] = True
+        # Builds the initial response object and
+        # Pulls the phone number from the request parameters
+        exist_response = {
+            'exists': False
+        }
+        phone_number_input = params['phoneNumber']
+        current_app.logger.info('Checking if user with phone number {0} exists'.format(phone_number_input))
 
+        # Makes a call against the database to find a user based on the phone number filter and
+        # Sets the response object field 'exists' to True if a user is found
+        user_by_phone = User.query.filter_by(phone_number=params['phoneNumber'])
+        if user_by_phone.first() != None:
+            exist_response['exists'] = True
+
+        # Logs whether a user has been found and
+        # Returns the response
+        current_app.logger.info('Found {0} user(s) with phone number {1}'.format(user_by_phone.count(), phone_number_input))
         return jsonify(exist_response), 200
     except Exception as e:
-        print(str(e))
+        # Logs the response and
+        # Returns a 500 response (Internal Server Error)
+        current_app.logger.fatal(str(e))
         return error_response(500)
 
 
 @bp.route('/user/create', methods=['POST'])
 def create_user():
     try:
+        # Loads the request body into a json format and
+        # Checks if all required parameters are included in the request
         request_data = json.loads(request.data)
         if ('firstName' not in request_data or
             'lastName' not in request_data or
-            'email' not in request_data or
             'phoneNumber' not in request_data or
             'password' not in request_data):
+            current_app.logger.error('request body not formatted correctly, body is missing required parameters.')
             return error_response(400)
 
+        # Creates a User object and creates the user from the request body json
         user = User()
         user.from_dict(request_data, new_user=True)
+
+        # Logs that the user is being added to the database and then adds to the database
+        # We will commit later once everything has been processed correctly
         db.session.add(user)
-        db.session.commit()
+        current_app.logger.info('added user with phone number {0} to the database session'.format(user.phone_number))
 
+        # Formats the request response to a json format to be returned in the response
         response = jsonify(user.to_dict())
-        response.status_code = 201
-        return response
 
+        # Commits the user to the database and logs that is has been commited
+        db.session.commit()
+        current_app.logger.info('commited user with phone number {0} to the database session'.format(user.phone_number))
+
+        # Returns the response with status code 201 to indicate the user has been created
+        return response, 201
     except Exception as e:
+        # Logs the exception that has been raised and rolls back all the changes made
+        current_app.logger.fatal(str(e))
+        db.session.rollback()
+        # Returns a 500 response (Internal Server Error)
         return error_response(500)
--- app/models.py
+++ app/models.py
@@ -9,12 +9,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(32))
     last_name = db.Column(db.String(32))
-    email = db.Column(db.String(128), index=True, unique=True)
     phone_number = db.Column(db.String(32), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User: {}>'.format(self.email)
+        return '<User: {0}>'.format(self.email)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -22,22 +21,19 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def to_dict(self, include_email=False):
+    def to_dict(self):
         data = {
             'id': self.id,
             'firstName': self.first_name,
             'lastName': self.last_name,
             'phoneNumber': self.phone_number
         }
-        if include_email:
-            data['email'] = self.email
         return data
 
     def from_dict(self, data, new_user=False):
         self.first_name = data['firstName']
         self.last_name = data['lastName']
         self.phone_number = data['phoneNumber']
-        self.email = data['email']
 
         if new_user and 'password' in data:
             self.set_password(password=data['password'])
