430dc506ab139d87012aba89e0b8c6bb380f862b
David Acevedo <dacevedo.cs@gmail.com>
2020-02-07 19:41:15 -0500

    Fixed config freeze

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'super-secret'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret'
     JWT_ACCESS_TOKEN_EXPIRES = 1800
     TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
     TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
